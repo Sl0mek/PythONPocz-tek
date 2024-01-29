f18fcf0d05df72386857e1fdaf158faca69b46dd
Andrzej Słomka <andrzejslomkapraca@gmail.com>
2024-01-29 21:10:29 +0100

    Update DataGenerator.py

--- Shop/DataGenerator.py
+++ Shop/DataGenerator.py
@@ -6,14 +6,14 @@ class DataGenerator:
 
     @classmethod
     def generate_base_order(cls):
-        return [OrderElement(product=Product(name="Procesor", category_name="Computer", unite_price=1000), quantity=1),
-                OrderElement(product=Product(name="Graphic card", category_name="Computer", unite_price=1200),
+        return [OrderElement(product=Product(name="Procesor", identifier="1", category_name="Computer", unite_price=1000), quantity=1),
+                OrderElement(product=Product(name="Graphic card", identifier="2", category_name="Computer", unite_price=1200),
                              quantity=1),
-                OrderElement(product=Product(name="RAM", category_name="Computer", unite_price=700), quantity=1),
-                OrderElement(product=Product(name="Disc", category_name="Computer", unite_price=200), quantity=1),
-                OrderElement(product=Product(name="Power suply", category_name="Computer", unite_price=300),
+                OrderElement(product=Product(name="RAM", identifier="23", category_name="Computer", unite_price=700), quantity=1),
+                OrderElement(product=Product(name="Disc", identifier="12", category_name="Computer", unite_price=200), quantity=1),
+                OrderElement(product=Product(name="Power suply", identifier="332", category_name="Computer", unite_price=300),
                              quantity=1),
-                OrderElement(product=Product(name="Cooling", category_name="Computer", unite_price=100), quantity=1)]
+                OrderElement(product=Product(name="Cooling", identifier="2656", category_name="Computer", unite_price=100), quantity=1)]
 
     @classmethod
     def generate_rnd_order(cls, quantity=None):
@@ -24,5 +24,5 @@ class DataGenerator:
             quantity = cls.MAX_ORDER_ELEMENTS
         for i in range(1, quantity + 1):
             order_elements.append(
-                OrderElement(Product(name=f"Product {i}", category_name=f"Category {i}", unite_price=i), quantity=i))
+                OrderElement(Product(name=f"Product {i}", identifier=f"ID {i}", category_name=f"Category {i}", unite_price=i), quantity=i))
         return order_elements
